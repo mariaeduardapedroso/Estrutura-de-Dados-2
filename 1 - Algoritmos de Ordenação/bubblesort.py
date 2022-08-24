f5d330b0931d78e8aa17dc3cc3ab7c5f8e07978c
Maria Eduarda Pedroso <mpedroso@alunos.utfpr.edu.br>
2022-08-23 22:42:09 -0300

    Update bubblesort.py

--- 1 - Algoritmos de Ordenação/bubblesort.py
+++ 1 - Algoritmos de Ordenação/bubblesort.py
@@ -1,1 +1,24 @@
-print('madu')
+
+def ordenarBubbleSort(v,tipo):
+    tamanhoVetor = len(v)
+    trocou = True
+    iteracao = 0
+    while(trocou):
+        trocou=False
+        for i in range(tamanhoVetor-1):
+            iteracao= iteracao+1
+            if v[i]>v[i+1]:
+                v[i],v[i+1]=v[i+1],v[i]
+                trocou=True
+    tamanhoVetor=tamanhoVetor-1
+    print(iteracao)
+        
+
+
+
+v=[1,4,2,3,5,6]
+tipo = 1
+
+print(v)
+ordenarBubbleSort(v,2)
+print(v)
