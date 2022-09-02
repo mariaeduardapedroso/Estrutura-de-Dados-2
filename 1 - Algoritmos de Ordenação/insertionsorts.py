c4773121fdb2aa5be58faf86fefaec3094687105
Maria Eduarda Pedroso <a2150336@vmpdc.utfpr>
2022-09-02 18:00:26 -0300

    comentando insertionsort

--- 1 - Algoritmos de Ordenação/insertionsorts.py
+++ 1 - Algoritmos de Ordenação/insertionsorts.py
@@ -1,15 +1,19 @@
 def ordenarInsertionSort(v,tipoCrescente):
+    #ir rodando o vetor para a esquerda e conseguir voltar comparando
     for posicao in range(1,len(v)):
         auxiliar = v[posicao]
         posicaocomparacao = posicao-1
         if tipoCrescente:
+            #parte para comparar e ir dando shift e voltando o vetor
             while posicaocomparacao >= 0 and auxiliar < v[posicaocomparacao]:
                 v[posicaocomparacao+1] = v[posicaocomparacao]
                 posicaocomparacao = posicaocomparacao - 1
         else:
+            #parte para comparar e ir dando shift e voltando o vetor
             while posicaocomparacao >= 0 and auxiliar > v[posicaocomparacao]:
                 v[posicaocomparacao+1] = v[posicaocomparacao]
                 posicaocomparacao = posicaocomparacao - 1
+        #trocar a variavel do indice que comecamos para depois dos shifts
         v[posicaocomparacao+1]=auxiliar
 
 
